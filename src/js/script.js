b6fda7ee5efdf36b16448c98a80f089a80783118
Artem <ap3994741@gmail.com>
2025-05-04 00:20:33 +0300

    made header (breakpoint: 360px)

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,15 @@
-import '/src/sass/style.scss'
+import '/src/sass/style.scss';
+
+const burgerBtn = document.querySelector(".burger"),
+    menu = document.querySelector(".header__menu"),
+    closeBtn = document.querySelector(".header__menu-close");
+
+burgerBtn.addEventListener("click", () => {
+    menu.classList.add("header__menu_active"),
+        document.body.style.overflow = "hidden";
+});
+
+closeBtn.addEventListener("click", () => {
+    menu.classList.remove("header__menu_active"),
+        document.body.style.overflow = "";
+});
